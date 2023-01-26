b24ecca9aea4c0dd8bf664bb07248fa806ae2349
Aryan <aryan16072001236809@gmail.com>
2023-01-26 22:33:48 +0530

    Rendering new Products by clicking on Different Pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 function App() {
 
   const [products, setProducts] = useState([])
-  const [page, SetPage] = useState(1)
+  const [page, setPage] = useState(1)
 
   const fetchProducts = async () => {
     const res = await fetch('https://dummyjson.com/products?limit=100')
@@ -18,17 +18,32 @@ function App() {
     fetchProducts()
   }, [])
 
+  const selectPageHandler = (selectedPage) => {
+    setPage(selectedPage)
+  }
   return (
     <div>
       {
         products.length > 0 && <div className='products'>{
           products.slice(page * 10 - 10, page * 10).map((prod) => {
-            return <span className='products__single' key={prod.id}>
+            return (<span className='products__single' key={prod.id}>
               <img src={prod.thumbnail} alt={prod.title} />
               <span>{prod.title}</span>
             </span>
+            )
           })
         }</div>
+      }
+      {
+        products.length > 0 && <div className='pagination'>
+          <span>◀️</span>
+          {
+            [...Array(products.length / 10)].map((_, i) => {
+              return <span onClick={() => selectPageHandler(i + 1)} key={i}>{i + 1}</span>
+            })
+          }
+          <span>▶️</span>
+        </div>
       }
     </div>
 
